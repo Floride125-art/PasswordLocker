8bd5974cc3e4f3929b2488e28874d42c9a6d3eb4
Floride125-art <FTuyisenge120@daviscollege.com>
2021-03-08 01:17:40 +0200

    create a function of creating, saving, and saving users

--- run.py
+++ run.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3.6
 from passLocker import User, Credential
+def create_user(username, password):
+    '''
+    function to create a user
+    '''
+    user = User(username,password)
+    return user
+
+def save_user(user):
+    '''
+    function to save a user
+    '''
+    user.save_user()
+
+def list_users():
+    '''
+    function to display all users
+    '''
+    return User.list_users()
