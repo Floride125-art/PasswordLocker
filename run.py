953a0717f0741cd166b697f97635220e78fe609e
Floride125-art <FTuyisenge120@daviscollege.com>
2021-03-08 01:19:45 +0200

    create function to check, save, and delete credentials

--- run.py
+++ run.py
@@ -18,3 +18,28 @@ def list_users():
     function to display all users
     '''
     return User.list_users()
+def user_checker(username,password):
+    '''
+    to check if user exist
+    '''
+    user_checker = Credential.user_checker(username,password)
+    return user_checker
+
+def create_credential(username,password,account_name):
+    '''
+    creating new account credential
+    '''
+    credential = Credential(username,password,account_name)
+    return credential
+
+def save_credential(credential):
+    '''
+    saving account credential
+    '''
+    credential.save_credential()
+
+def delete_credential(credential):
+    '''
+    deleting an account credential
+    '''
+    credential.delete_credential()
