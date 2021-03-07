22cf6ebca9af1cec2ab6015003a7ed7ace0634e5
Floride125-art <FTuyisenge120@daviscollege.com>
2021-03-08 00:49:25 +0200

    add method to display all user credentials

--- passLocker.py
+++ passLocker.py
@@ -1,17 +1,24 @@
+import random
 class User:
     '''
     create a user class
     '''
     users = []
-def __init__(self,username,password):
-        '''
-        generate new instance of a class user
-        '''
+    def __init__(self,username,password):
+            '''
+            generate new instance of a class user
+            '''
 
-        self.username = username
-        self.password = password
-def save_user(self):
-        '''
-        function to save a user credentials
-        '''
-        User.users.append(self)
+            self.username = username
+            self.password = password
+    def save_user(self):
+            '''
+            function to save a user credentials
+            '''
+            User.users.append(self)
+    @classmethod #This means you can use the class and its properties inside that method rather than a particular instance.
+    def list_users(cls):
+            '''
+            method to display all credentials
+            '''
+            return cls.users
