da9b82ff138873684ea33392064a0a861da89616
Floride125-art <FTuyisenge120@daviscollege.com>
2021-03-08 00:41:55 +0200

    define username and password

--- passLocker.py
+++ passLocker.py
@@ -3,3 +3,10 @@ class User:
     create a user class
     '''
     users = []
+def __init__(self,username,password):
+        '''
+        generate new instance of a class user
+        '''
+
+        self.username = username
+        self.password = password
