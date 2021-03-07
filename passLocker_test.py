fe2de8e8f39cc5c0db522d449965f6bf7aa3ddff
Floride125-art <FTuyisenge120@daviscollege.com>
2021-03-08 01:00:54 +0200

    create a class for test and add function that runs before

--- passLocker_test.py
+++ passLocker_test.py
@@ -1,2 +1,11 @@
 import unittest
 from passLocker import User, Credential
+class User_Test(unittest.TestCase):
+    '''
+    class to test an user class
+    '''
+    def setUp(self):
+        '''
+        function that runs before
+        '''
+        self.user = User('Floride','Tuyisenge')
