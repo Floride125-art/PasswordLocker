a884c138034766dba86db268a3dbca9333c7fccf
Floride125-art <FTuyisenge120@daviscollege.com>
2021-03-08 01:13:39 +0200

    check to test multiple credentials

--- passLocker_test.py
+++ passLocker_test.py
@@ -52,4 +52,53 @@ class Credential_Test(unittest.TestCase):
         method that clean after each test runned.
         '''
         Credential.credentials = []
+    def test_save_multiple_accounts(self):
+        '''
+        test to check if we can save multiple credentials 
+        '''
+        self.credential.save_credential()
+        test_credential = Credential("Facebook","Denyse","dduny") 
+        test_credential.save_credential()
+        self.assertEqual(len(Credential.credentials),2)
+
+    def test_deleted_credential(self):
+        """
+        test if we can delete account credential
+        """
+        self.credential.save_credential()
+        test_credential = Credential("Facebook","Denyse","dduny") 
+        test_credential.save_credential()
+        self.credential.delete_credential()
+        self.assertEqual(len(Credential.credentials),1) 
+
+    def test_search_credential(self):
+        """
+        test to check if we can find an account credential
+        """
+        self.credential.save_credential()
+        test_credential = Credential("Facebook","Denyse","dduny")  
+        test_credential.save_credential()
+        found = Credential.search_credential("Istagram")
+        self.assertEqual(found.account_name,test_credential.account_name)
+        
+    def test_credential(self):
+        """
+        check if a credential account exist
+        """
+        self.credential.save_credential()
+        found = Credential("Facebook","Denyse","dduny")  
+        found.save_credential()
+        credential_found = Credential.search_credential("Facebook")
+        self.assertTrue(credential_found)
+
+    def test_view_all_credential(self):
+        '''
+        test  if credential can be viewed
+        '''
+
+        self.assertEqual(Credential.view_all_credential(),Credential.credentials)
+
+if __name__ == '__main__':
+    unittest.main()
+        
 
