e4424bb760295543f33ffca6649f5dbff9ece18c
Floride125-art <FTuyisenge120@daviscollege.com>
2021-03-08 00:43:23 +0200

    define function that will save user credentials

--- passLocker.py
+++ passLocker.py
@@ -10,3 +10,8 @@ def __init__(self,username,password):
 
         self.username = username
         self.password = password
+def save_user(self):
+        '''
+        function to save a user credentials
+        '''
+        User.users.append(self)
